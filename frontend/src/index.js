7365e810f0b9c58584802dc82be0c18955a3fc71
Shyampatil07 <shyamlade06@gmail.com>
2025-11-24 00:22:59 +0530

    add files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import HomePage from './landingPage/home/HomePage';
+import Signup from './landingPage/signup/Signup';
+import Footer from './landingPage/Footer';
+import Navbar from './landingPage/Navbar';
+import AboutPage from './landingPage/about/AboutPage';
+import ProductPage from './landingPage/products/ProductPage';
+import PricingPage from './landingPage/pricing/PricingPage';
+import SupportPage from './landingPage/support/SupportPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <HomePage/>
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+
+    <Route path="/" element={<HomePage/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+    <Route path="/about" element={<AboutPage/>}></Route>
+    <Route path="/product" element={<ProductPage/>}></Route>
+    <Route path="/pricing" element={<PricingPage/>}></Route>
+    <Route path="/support" element={<SupportPage/>}></Route>
+    
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
+  
 );
--- frontend/src/landingPage/home/Awards.js
+++ frontend/src/landingPage/home/Awards.js
@@ -2,7 +2,46 @@ import React from 'react';
 
 function Awards() {
     return (  
-        <h1>Awards</h1>
+       <div className='container mt-5'> 
+           <div className='row'>
+                <div className='col-6 p-5'>
+                  <img src='media/largestBroker.svg'/>
+                </div>
+                <div className='col-6 p-5 mt-5'>
+                    <h1>largest Stock Broker in India </h1>
+                    <p className='mb-5'>That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments, making us India’s largest broker; contributing to 15% of daily retail exchange volumes in India.</p>
+                    <div className='row'>
+                         <div className='col-6'>
+                               <ul>
+                        <li>
+                            <p>futures and Options</p>
+                        </li>
+                         <li>
+                            <p>Commodity derivatives</p>
+                        </li>
+                         <li>
+                            <p>Currency derivatives</p>
+                        </li>
+                        </ul>
+                         </div>
+                        <div className='col-6'>
+                                  <ul>
+                        <li>
+                            <p>stocks & IPOs</p>
+                        </li>
+                         <li>
+                            <p>Direct mutual funds</p>
+                        </li>
+                         <li>
+                            <p>Bonds and security</p>
+                        </li>
+                        </ul>
+                        </div> 
+                    </div>
+                   <img src='media/pressLogos.png' style={{width:"90%"}}/>
+                </div>
+           </div>
+       </div>
     );
 }
 
