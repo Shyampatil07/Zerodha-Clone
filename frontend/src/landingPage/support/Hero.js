736ac2f83471b89f6ef099eb1bcaa617b8cefc38
Shyampatil07 <shyamlade06@gmail.com>
2025-11-24 22:59:24 +0530

    add files

--- frontend/src/landingPage/support/Hero.js
+++ frontend/src/landingPage/support/Hero.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <section className='container-fluid' id='supporthero'>
+            <div className='p-5 ' id='wrapper'>
+                <h4>Support Portal</h4>
+                <a href=''>Track Tickets</a>
+            </div>
+            <div className='row p-3 m-3'>
+                <div className='col-6 p-5 '>
+                    <h1 className='fs-3'>Search for an answer or brower help topics to create a ticket</h1>
+                    <input placeholder='Eg. how do I activate F&0' /><br></br>
+                    <a href=''>Track account opening</a>&nbsp;&nbsp;&nbsp;
+                    <a href=''>Track Segment activation</a>&nbsp;&nbsp;&nbsp;
+                    <a href=''>Intraday margins</a>&nbsp;&nbsp;&nbsp;
+                    <a href=''>Kite user manual</a>&nbsp;&nbsp;
+                </div>
+                <div className='col-6  p-5'>
+                    <h1 className='fs-3 '>Featured</h1>
+                    <ol>
+                        <li>
+                            <a href=''>Current Takeovers and Delisting - January 2024</a>
+                        </li>
+                        <li>
+                            <a href=''>Latest intraday leverages- MIS & CO</a>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
